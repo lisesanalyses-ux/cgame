11749738e4d32a6642ad61d6df6c3209ffa59092
lisesanalyses-ux <lisesanalyses@gmail.com>
2025-09-15 11:37:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const gameBoard = document.getElementById('game-board');
+const cards = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+let flippedCards = [];
+let matchedCards = [];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function createBoard() {
+    const shuffledCards = shuffle(cards);
+    for (let i = 0; i < shuffledCards.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.value = shuffledCards[i];
+        card.addEventListener('click', flipCard);
+        gameBoard.appendChild(card);
+    }
+}
+
+function flipCard() {
+    if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
+        this.classList.add('flipped');
+        this.textContent = this.dataset.value;
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) {
+            setTimeout(checkForMatch, 1000);
+        }
+    }
+}
+
+function checkForMatch() {
+    const [card1, card2] = flippedCards;
+    if (card1.dataset.value === card2.dataset.value) {
+        matchedCards.push(card1, card2);
+        card1.removeEventListener('click', flipCard);
+        card2.removeEventListener('click', flipCard);
+    } else {
+        card1.classList.remove('flipped');
+        card2.classList.remove('flipped');
+        card1.textContent = '';
+        card2.textContent = '';
+    }
+    flippedCards = [];
+}
+
+createBoard();
